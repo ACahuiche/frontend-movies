df1a6e33f8cb7e71396b9f20fe06f8c17debddc0
ACahuiche <alberto.cahuiche@outlook.com>
2023-10-09 21:25:49 -0600

    BF-2: The page not found component was created and added to the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '**', component: NotfoundComponent}
 ];
 
 @NgModule({
